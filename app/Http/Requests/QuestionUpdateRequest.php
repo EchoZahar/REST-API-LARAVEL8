81c296f377d6c74970ae8d188af58af22d6eaa72
EchoZahar <zahar.yamal@yandex.ru>
2021-06-30 03:17:50 +0400

    Валидация из методов create и update перенесена в отдельные файлы.

---
+++ app/Http/Requests/QuestionUpdateRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Question;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class QuestionUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'      => ['required', 'max:50'],
+            'email'     => ['required', 'email', 'min:5', 'max:100'],
+            'message'   => ['required', 'min:5', 'max:2000'],
+            // только зарегастрированный пользователь
+            'status'    => ['required', Rule::In(Question::$types)],
+            'dateStart' => ['nullable','date'],
+            'comment'   => ['required', 'min:5', 'max:500'],
+            'user_id'   => ['nullable']
+        ];
+    }
+}
